4a03cc4d854ac5062ca3dd30bb506714b7cbd95f
Shreyapatil <shreyapatil062003@gmail.com>
2026-02-04 23:47:29 +0530

    Final Cleanup of submodules and root files

---
+++ 01-Basics/RightHalfPyramid.java
@@ -0,0 +1,14 @@
+//Day 1
+//Print the Pattern using Print Statements
+
+public class RightHalfPyramid {
+
+    public static void main(String [] args) {
+        
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    }
+}
