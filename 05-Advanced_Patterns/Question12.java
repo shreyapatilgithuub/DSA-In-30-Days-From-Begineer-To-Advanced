ac07265c1b74fb4551ecfe2acbd475f9711f8e0a
Shreyapatil <shreyapatil062003@gmail.com>
2026-02-08 18:36:11 +0530

    Day 5: Practicing advanced Patterns questions

---
+++ 05-Advanced_Patterns/Question12.java
@@ -0,0 +1,28 @@
+/* Print the pattern
+     1
+     22
+     333
+     4444
+     55555  */
+
+public class Question12 {
+   public static void main(String args[]) {
+       int n = 5;
+
+       for(int i=1; i<=n; i++) {
+
+           //spaces
+
+           for(int j=1; j<=n-i; j++) {
+               System.out.print(" ");
+           }
+
+           //numbers
+
+           for(int j=1; j<=i; j++) {
+               System.out.print(i+" ");
+           }
+           System.out.println();
+       }
+   }  
+}     
