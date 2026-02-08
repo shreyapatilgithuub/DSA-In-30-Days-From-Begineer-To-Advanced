c723123b317d2afeed4c0cf948d09309a6a802f9
Shreyapatil <shreyapatil062003@gmail.com>
2026-02-08 18:40:54 +0530

    Day 5: Advanced Patterns Questions

---
+++ 05-Advanced_Patterns/Question13.java
@@ -0,0 +1,34 @@
+/* Print the pattern
+        1
+       212     
+      32123
+     4321234
+    543212345 */
+
+    public class Question13 {
+   public static void main(String args[]) {
+       int n = 5;
+
+       for(int i=1; i<=n; i++) {
+
+           //spaces
+
+           for(int j=1; j<=n-i; j++) {
+               System.out.print(" ");
+           }
+
+           //first part
+
+           for(int j=i; j>=1; j--) {
+               System.out.print(j);
+           }
+
+           //second part
+
+           for(int j=2; j<=i; j++) {
+               System.out.print(j);
+           }
+           System.out.println();
+       }
+   }  
+}
