498d81ce95fb99ccb83b3b780bd28a555e799ac3
Shreyapatil <shreyapatil062003@gmail.com>
2026-02-12 22:23:10 +0530

    Refactoring search logic
    Co-authored-by: shreyaharshini <patilshreya06harshini@gmail.com>

--- 09-Arrays/BinarySearch.java
+++ 09-Arrays/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch {
         // A sorted array is required for Binary Search
         int[] numbers = {10, 20, 30, 40, 50};
         
-        System.out.print("Enter the number to search for: ");
+        System.out.print("Enter the number to search: ");
         int target = sc.nextInt();
         
         int low = 0;
